dcb37380ec2941422c2884d6dbf23b200387f3d6
DThorat <DThorat@192.168.0.8>
2018-12-05 17:31:05 +0530

    [Datta]-Updated for routing to child item.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ModuleWithProviders } from "@angular/core";
+import { ItemDetailComponent } from './item/item.component';
+import { AppComponent } from './app.component';
+import { ItemListComponent } from './item-list/item-list.component'; 
+import { GeoItemListComponent } from './geo-item-list/geo-item-list.component'; 
+import { PageNotFoundComponent }    from './page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+					{ path: '', component: ItemListComponent },
+					{ path: "item/:id", component: ItemDetailComponent },
+					{ path: 'items', component: ItemListComponent },
+					{ path: 'geoItems', component: GeoItemListComponent},
+					{ path: '**', component: PageNotFoundComponent }
+			  ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,18 @@ import { HttpModule } from '@angular/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SliderModule } from 'angular-image-slider';
+import { ItemDetailComponent } from './item/item.component';
+import { PageNotFoundComponent }   from './page-not-found/page-not-found.component';
+import { ItemListComponent } from './item-list/item-list.component'; 
+import { GeoItemListComponent } from './geo-item-list/geo-item-list.component'; 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ItemDetailComponent,
+    ItemListComponent,
+    GeoItemListComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
